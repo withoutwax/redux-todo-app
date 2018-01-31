0873da3fb266552a3d727f9ac13bc9726dcc523b
Will Kim <rlagmlckd@gmail.com>
2018-01-31 14:15:57 -0600

    Attempt to reorganize the redux architecture (3/3) - Lists now shows

--- src/containers/TodoLists.js
+++ src/containers/TodoLists.js
@@ -2,26 +2,21 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class TodoLists extends Component {
+
   render() {
 
-    // let listOfItems;
-    //
-    // if (valueEntered) {
-    //   listOfItems = this.props.todo.map((items, i) => <li key={i} >{items.text.list}</li>);
-    // }
-
-
-    console.log(this.props);
-    if (this.props.todo.listInitiated) {
-      // console.log(this.props.todo[0]);
-      console.log("This is working!");
-      // listOfItems = this.props.todo.map((items, i) => <li key={i} >{items.text.list}</li>);
+    let listOfItems;
+    
+    if (this.props.todo[0] !== undefined) {
+      console.log(this.props.todo[0].text.list);
+      // console.log('test');
+      listOfItems = this.props.todo.map((items, i) => <li key={i} >{items.text.list}</li>);
     }
 
     return (
       <div>
         Todo lists:
-        {/* {listOfItems} */}
+        {listOfItems}
       </div>
     );
   }
